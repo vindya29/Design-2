fa7e9a104c7570ecaaac0d808313dbdcc86cd2dd
vindya <69504272+vindya29@users.noreply.github.com>
2025-03-19 16:19:31 -0500

    Update queueUsingStack.java

--- queueUsingStack.java
+++ queueUsingStack.java
@@ -1,1 +1,49 @@
+class MyQueue {
 
+    Stack<Integer> inSt;
+    Stack<Integer> outSt;
+    public MyQueue() {
+        this.inSt = new Stack<>();
+         this.outSt = new Stack<>();
+        
+    }
+    
+    public void push(int x) {
+        inSt.push(x);
+        
+    }
+    
+    public int pop() {
+        if(outSt.isEmpty()){
+            while(!inSt.isEmpty()){
+                outSt.push(inSt.pop());
+            }
+        }
+        return outSt.pop();
+        
+    }
+    
+    public int peek() {
+        if(outSt.isEmpty()){
+            while(!inSt.isEmpty()){
+                outSt.push(inSt.pop());
+            }
+        }
+        return outSt.peek();
+        
+    }
+    
+    public boolean empty() {
+        return inSt.isEmpty() && outSt.isEmpty();
+        
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
